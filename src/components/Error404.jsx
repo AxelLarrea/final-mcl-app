f5a25acae0153b2e463a5656e8015328648a548b
Axel Larrea <63266736+AxelLarrea@users.noreply.github.com>
2021-11-15 12:20:12 -0300

    15-11-21 "Completely Working"

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,7 +1,9 @@
+import error from '../assets/images/error.svg';
 
 const Error404 = () => {
     return (
-        <div>
+        <div className="error-container">
+            <img src={error} alt="sad face"/>
             <h1>404</h1>
             <h3>Not Found</h3>
         </div>
